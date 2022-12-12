891d3e0f23a2d74aabcbbf4aaad462c474e931d5
nisha <nishantsingh187@gmail.com>
2022-12-12 05:58:16 +0530

    Welcome To Address Book Program

---
+++ src/BridgeLabz/AddressBookSystemMain.java
@@ -0,0 +1,9 @@
+package BridgeLabz;
+
+public class AddressBookSystemMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book\n" +
+                "Program in\n" +
+                "AddressBookMain class");
+    }
+}
