5dab63dd9d3a7acd38542517d11d8452461b637a
nisha <nishantsingh187@gmail.com>
2022-12-12 06:08:43 +0530

    Add new Contact

--- src/BridgeLabz/AddressBookSystemMain.java
+++ src/BridgeLabz/AddressBookSystemMain.java
@@ -2,8 +2,12 @@ package BridgeLabz;
 
 public class AddressBookSystemMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book\n" +
-                "Program in\n" +
-                "AddressBookMain class");
+
+        System.out.println("Welcome to address book system using collection");
+
+        AddressBookSystemDetails details = new AddressBookSystemDetails();
+
+        details.addDetails();
+        details.display();
     }
 }
