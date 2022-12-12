dedd394f1de253de417048ac1780632b86412ca0
nisha <nishantsingh187@gmail.com>
2022-12-12 06:13:46 +0530

    Edit Existing Contact

--- src/BridgeLabz/AddressBookSystemMain.java
+++ src/BridgeLabz/AddressBookSystemMain.java
@@ -1,13 +1,33 @@
 package BridgeLabz;
 
+
+import java.util.Scanner;
+
 public class AddressBookSystemMain {
     public static void main(String[] args) {
-
         System.out.println("Welcome to address book system using collection");
+        Scanner sc = new Scanner(System.in);
 
         AddressBookSystemDetails details = new AddressBookSystemDetails();
 
-        details.addDetails();
-        details.display();
+        int choice = 1;
+        while (choice == 1) {
+            System.out.println("Choose One of the Given Choices as per Requirement: ");
+            System.out.println("1.Add Details\n2.Edit Details\n3.Display Details\n0.Exit");
+
+            switch (sc.nextInt()) {
+                case 1:
+                    details.addDetails();
+                    break;
+                case 2:
+                    details.editDetails();
+                    break;
+                case 3:
+                    details.display();
+                    break;
+                default:
+                    choice = 0;
+            }
+        }
     }
 }
